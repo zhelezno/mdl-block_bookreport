432c9019b84701d349cffb87751bf0f1c5306f59
Андрей Часников <chasnikov.aa@zhcom.ru>
2021-03-24 18:18:01 +0300

    Выгрузка проекта 24.03.2021 18:17; Смена названия плагина, приветствия

--- lang/en/block_bookreport.php
+++ lang/en/block_bookreport.php
@@ -22,7 +22,7 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-$string['pluginname'] = 'Книжки';
+$string['pluginname'] = 'Эксперт книги';
 $string['bookreport'] = 'Отчет';
 $string['bookreports'] = 'Отчеты';
 $string['shortpluginname'] = 'Книжки';
